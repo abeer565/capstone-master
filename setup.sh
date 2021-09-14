5ecf0862e951bd49fc1543be4a6fdd5ae0d17cd0
abeer <53477589+abeer565@users.noreply.github.com>
2021-09-14 19:12:57 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 export AUTH0_DOMAIN="dev-1qgup88m.us.auth0.com"
 export ALGORITHMS=['RS256']
-export API_AUDIENCE='image'
+export API_AUDIENCE='tenant'
 export DATABASE_URL='postgres://lcwsqlaqeedlfs:6fd3cb209bb18ac0d99eaae1d7a451e8e8e209e777aa720a90a00653973b9c9d@ec2-54-147-126-173.compute-1.amazonaws.com:5432/d55fftrn0iufhr'
 export HEROKU_POSTGRESQL_NAVY_URL = 'postgres://judhlvwqkfcsrk:60299cb30ad31a969be45479eca81a9c65c1f0000d440e7702c72df460524409@ec2-52-4-111-46.compute-1.amazonaws.com:5432/d6igdki6aek4a6'
